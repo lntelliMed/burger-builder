d1e482a1b4435af091e16bc20d26c38854245358
“Sam <sam.alsmadi@gmail.com>
2018-03-10 02:17:03 -0500

    Add Order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,24 @@ class BurgerBuilder extends Component {
                     cheese: 0,
                     meat: 0
                   },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  }
+
+  updatePurcahseState (ingredients) {
+    // const ingredients = {
+    //   ...this.state.ingredients
+    // };
+    const sum = Object.keys(ingredients)
+          .map(ingKey => {
+            return ingredients[ingKey];
+          })
+          .reduce((sum, el) => {
+            return sum + el;
+          }, 0);
+    this.setState({
+      purchasable: sum > 0
+    });
   }
 
   addIngredientHandler = (type) => {
@@ -41,6 +58,7 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
+    this.updatePurcahseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -62,6 +80,7 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
+    this.updatePurcahseState(updatedIngredients);
   }
 
   render () {
@@ -78,6 +97,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          purchasable={this.state.purchasable}
           price={this.state.totalPrice}
         />
       </Aux>
