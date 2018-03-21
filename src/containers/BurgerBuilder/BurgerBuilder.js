472c8320c66ebde846dbd82c62cdc9811d2fa455
“Sam <sam.alsmadi@gmail.com>
2018-03-21 15:22:21 -0400

    Send POST request to Firebase whenever user orders a burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -58,7 +59,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    // alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, // In prod, this should be calculated on server side so user can't manipulate this before sending!
+      customer: {
+        name: 'Joe Smith',
+        address: {
+          street: 'Test str.',
+          zipCode: '12345',
+          country: 'USA'
+        },
+        email: 'joe.smith@somecompany.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   };
 
   addIngredientHandler = (type) => {
