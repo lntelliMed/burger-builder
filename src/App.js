b0f10f8bd7f18610442eab1aa1beeadf56f7b857
“Sam <sam.alsmadi@gmail.com>
2018-03-22 15:18:47 -0400

    Eject axios interceptors whenever not needed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+  state = {
+    show: true
+  };
+  // componentDidMount () { // Used for testing interceptor cleanup!
+  //   setTimeout(() => {
+  //     this.setState({show: false});
+  //   }, 5000);
+  // }
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder>
-
-          </BurgerBuilder>
+          {this.state.show ? <BurgerBuilder /> : null}
         </Layout>
       </div>
     );
