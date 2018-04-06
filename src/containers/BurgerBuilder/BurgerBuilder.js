394b57776bcb88f4bf45b816b8286a41c0f69a08
“Sam <sam.alsmadi@gmail.com>
2018-04-05 22:34:59 -0400

    Allow users to submit orders from ContactData

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -70,32 +70,11 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // // alert('You continue!');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice, // In prod, this should be calculated on server side so user can't manipulate this before sending!
-    //   customer: {
-    //     name: 'Joe Smith',
-    //     address: {
-    //       street: 'Test st.',
-    //       zipCode: '12345',
-    //       country: 'USA'
-    //     },
-    //     email: 'joe.smith@somecompany.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // };
-    // axios.post('/orders.json', order)
-    //   .then(response => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
     const queryParams = [];
     for (const i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: './checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
   state = {
@@ -9,21 +11,56 @@ class ContactData extends Component {
     address: {
       street: '',
       postalCode: ''
-    }
+    },
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    // console.log(this.props.ingredients);
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price, // In prod, this should be calculated on server side so user can't manipulate this before sending!
+      customer: {
+        name: 'Joe Smith',
+        address: {
+          street: 'Test st.',
+          zipCode: '12345',
+          country: 'USA'
+        },
+        email: 'joe.smith@somecompany.com'
+      },
+      deliveryMethod: 'fastest'
+    };
+    axios.post('/orders.json', order)
+      .then(response => {
+        this.setState({ loading: false });
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
   }
 
   render () {
-    return (
-      <div className={classes.ContactData} >
-        <h4>Enter your Contact Data</h4>
+    let form = (
         <form>
           <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
           <input className={classes.Input} type="text" name="email" placeholder="Your Mail" />
           <input className={classes.Input} type="text" name="street" placeholder="Street" />
           <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-          <Button btnType="Success" >ORDER</Button>
-
+          <Button btnType="Success" clicked={this.orderHandler} >ORDER</Button>
         </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
+
+    return (
+      <div className={classes.ContactData} >
+        <h4>Enter your Contact Data</h4>
+        {form}
       </div>
     );
   }
