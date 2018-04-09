aefe34f030ef8473fbee41cbe2dead35bdb20f4d
“Sam <sam.alsmadi@gmail.com>
2018-04-09 01:53:08 -0400

    Add dropdown for delivery method

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,7 +3,7 @@ import classes from './Input.css';
 
 const input = (props) => {
   let inputElement = null;
-  switch (props.elemenType) {
+  switch (props.elementType) {
     case ('input'):
       inputElement =
           <input
@@ -16,6 +16,17 @@ const input = (props) => {
           <textarea className={classes.InputElement}
               {...props.elementConfig}
               value={props.value}  />
+      break;
+    case ('select'):
+      inputElement =
+        <select className={classes.InputElement}
+          value={props.value} >
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
       break;
     default:
       inputElement =
