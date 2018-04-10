87815314e285f06f617e5a8aba65c80ada98c6f8
“Sam <sam.alsmadi@gmail.com>
2018-04-10 01:38:35 -0400

    Disable order button if form is invalid

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,9 +83,11 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest'}
           ]
         },
-        value: ''
+        value: '',
+        valid: true
       }
     },
+    formIsValid: false,
     loading: false
   }
 
@@ -142,9 +144,15 @@ class ContactData extends Component {
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    console.log(updatedFormElement);
+    // console.log(updatedFormElement);
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
     this.setState({
-      orderForm: updatedOrderForm
+      orderForm: updatedOrderForm,
+      formIsValid
     });
   }
 
@@ -169,7 +177,7 @@ class ContactData extends Component {
                 touched={formElement.config.touched}
                 changed={(event) => this.inputChangedHandler(event, formElement.id)} />
           ))}
-          <Button btnType="Success" >ORDER</Button>
+          <Button btnType="Success" disabled={!this.state.formIsValid} >ORDER</Button>
         </form>
     );
     if (this.state.loading) {
