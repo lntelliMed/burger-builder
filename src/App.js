e2a9ccbcf1f0b514fe44e5f22be90c41896736c0
“Sam <sam.alsmadi@gmail.com>
2018-05-01 15:01:21 -0400

    Store auth state in localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -6,16 +8,17 @@ import Orders from './containers/Orders/Orders';
 import { Route, Switch }  from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 class App extends Component {
   state = {
     show: true
   };
-  // componentDidMount () { // Used for testing interceptor cleanup!
-  //   setTimeout(() => {
-  //     this.setState({show: false});
-  //   }, 5000);
-  // }
+
+  componentDidMount () {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
     return (
       <div>
@@ -36,4 +39,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
