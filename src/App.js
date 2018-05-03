4e6a91d66d51ec48394d95c00465e84125557c3d
“Sam <sam.alsmadi@gmail.com>
2018-05-02 21:34:05 -0400

    Guard routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-import { Route, Switch, withRouter }  from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect }  from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
@@ -20,19 +20,29 @@ class App extends Component {
   }
 
   render() {
+    let routes = (
+      <Switch >
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch >
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch >
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch >
+      );
+    }
     return (
       <div>
         <Layout>
-          {/* {this.state.show ? <BurgerBuilder /> : null} */}
-          {/* <BurgerBuilder />
-          <Checkout /> */}
-          <Switch >
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
@@ -45,4 +55,10 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
