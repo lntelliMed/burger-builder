c1ac9f5b5f0b7707112413dd9e7b0899ef7e433f
“Sam <sam.alsmadi@gmail.com>
2018-05-04 17:34:29 -0400

    Add another unit test for NavigationItems

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,10 +7,19 @@ import NavigationItems from './NavigationItems';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 configure({adapter: new Adapter()});
-
+let wrapper;
 describe('<Navigation Items >', () => {
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
+
+  it('should render three <NavigationItem /> elements if authenticated', () => {
+    // wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({isAuthenticated: true});
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
 });
